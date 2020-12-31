14b8a88d67d42ca7f9b8e449eb16cec8783afaae
akichan <4737990+ch1aki@users.noreply.github.com>
2020-12-31 22:31:40 +0900

    fix contacts

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,17 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/ch11aki',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/ch1aki',
   },
   {
     Comp: LinkedIn,
     alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
-  },
-  {
-    Comp: Envelope,
-    alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'https://www.linkedin.com/in/chiaki-sugawara-2b7605135/',
   },
 ]
 
