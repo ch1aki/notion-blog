facc6b7afd035d67f4e4e6c07958ffbc2ae4fdbd
akichan <4737990+ch1aki@users.noreply.github.com>
2020-12-31 22:34:49 +0900

    fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,11 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
+      <span>
+        <ExtLink href="https://ch1aki.com">
+          Powered by NotionBlog
+        </ExtLink>
+      </span>
     </footer>
   </>
 )
